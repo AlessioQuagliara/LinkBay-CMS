62e244803cf7e8907184a541240e926ac55a48f6
AlessioSpotex <alessio.quagliara@spotexsrl.it>
2024-07-30 00:13:28 +0200

    create route

--- routes.py
+++ routes.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, redirect, url_for
 from app import app
 
+# Store Routes -------------------------------------------------------------------------
 @app.route('/')
 def index():
     return render_template('index.html')
+# Admin Routes -------------------------------------------------------------------------
+@app.route('/admin/')
+def login():
+    return render_template('/admin/login.html')
+@app.route('/admin/logout')
+def logout():
+    return render_template('/admin/logout.html')
+@app.route('/admin/restore')
+def restore():
+    return render_template('/admin/restore.html')
