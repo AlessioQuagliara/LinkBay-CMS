6f60a5db72a5bc8c92bcdb146e9d33121541be40
AlessioSpotex <quagliara.alessio@outlook.com>
2025-04-12 14:48:14 +0200

    responsive

--- landing/__init__.py
+++ landing/__init__.py
@@ -2,4 +2,5 @@ from flask import Blueprint
 
 landing_bp = Blueprint('landing', __name__, template_folder='templates', static_folder='static')
 
-from . import routes, auth, superadmin  # importa i file dentro landing
+from . import routes, auth  # importa i file dentro landing
+
