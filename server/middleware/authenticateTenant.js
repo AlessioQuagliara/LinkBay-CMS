8b70a9dd657c497f341d4df9af55ae1043bbc0b9
Alessio Quagliara <quagliara.alessio@outlook.com>
2025-09-07 18:09:02 +0200

    Fix
    Restore project for issued

--- server/middleware/authenticateTenant.js
+++
@@ -1,5 +0,0 @@
-// No-op tenant resolver for initial scaffolding
-module.exports = function authenticateTenant(req, res, next) {
-  req.tenant = req.tenant || { name: 'default' };
-  return next();
-};
